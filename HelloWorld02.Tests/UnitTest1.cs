5cc53517f5268f57c1878c08dbd4937e0e1d6808
ccassel <ccassel@assurity.com>
2015-05-08 09:54:53 -0500

    Added dummy unit test

---
+++ HelloWorld02.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HelloWorld02.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string x = "Hello World";
+            Assert.AreEqual("Hello World", x);
+        }
+    }
+}
