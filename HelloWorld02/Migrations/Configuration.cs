61369ea888ef3f15c9def2d76fe86dfe1d35bc6c
ccassel <ccassel@assurity.com>
2015-05-09 07:43:24 -0500

    Enable-Migrations –EnableAutomaticMigration

---
+++ HelloWorld02/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace HelloWorld02.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<HelloWorld02.DAL.HelloWorld02Context>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            ContextKey = "HelloWorld02.DAL.HelloWorld02Context";
+        }
+
+        protected override void Seed(HelloWorld02.DAL.HelloWorld02Context context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
